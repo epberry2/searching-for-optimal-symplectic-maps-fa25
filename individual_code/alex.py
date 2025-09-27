24c649d2a07bcd9e966b3b08ceee4e29ee8f50b2
alextware <atware2@illinois.edu>
2025-09-27 17:49:39 -0500

    redid generating random points

--- individual_code/alex.py
+++ individual_code/alex.py
@@ -24,16 +24,17 @@ class Keyhole(Shape):
         self.angle = angle
 
 
-    def boundary_points(self, k1 = 1, k2 = 1, k3 = 1, seed = 0):
+    def boundary_points(self, k1 = 1, k2 = 1, k3 = 1, k4 = 1, seed = 0):
         rng = np.random.default_rng(seed)
-        theta = rng.uniform(self.angle, 2 * np.pi - self.angle, k1)
-        t1 = rng.uniform(0, 1, k2)
-        t2 = rng.uniform(0, 1, k3)
+        theta1 = rng.uniform(self.angle, 2 * np.pi - self.angle, k1)
+        theta2 = rng.uniform(self.angle, 2 * np.pi - self.angle, k2)
+        t1 = rng.uniform(0, 1, k3)
+        t2 = rng.uniform(0, 1, k4)
         
-        x_in = self.inner_radius * np.cos(theta) + self.position[0]
-        y_in = self.inner_radius * np.sin(theta) + self.position[1] 
-        x_out = self.outer_radius * np.cos(theta) + self.position[0]
-        y_out = self.outer_radius * np.sin(theta) + self.position[1]
+        x_in = self.inner_radius * np.cos(theta1) + self.position[0]
+        y_in = self.inner_radius * np.sin(theta1) + self.position[1] 
+        x_out = self.outer_radius * np.cos(theta2) + self.position[0]
+        y_out = self.outer_radius * np.sin(theta2) + self.position[1]
         a1 = (self.inner_radius - t1 * (self.inner_radius - self.outer_radius)) * np.cos(self.angle) + self.position[0]
         b1 = (self.inner_radius - t1 * (self.inner_radius - self.outer_radius)) * np.sin(self.angle) + self.position[1]
         a2 = (self.outer_radius - t2 * (self.outer_radius - self.inner_radius)) * np.cos(2 * np.pi - self.angle) + self.position[0]
@@ -48,10 +49,11 @@ keyhole = Keyhole(position=[0, 0], inner_radius=1, outer_radius=2, angle=np.pi/8
 
 # Generate boundary points
 # Number of boundary points to sample
-k1 = 500
-k2 = 100
-k3 = 100
-x_in, y_in, x_out, y_out, a1, b1, a2, b2 = keyhole.boundary_points(k1 = k1, k2 = k2, k3 = k3, seed=42)
+k1 = 200
+k2 = 200
+k3 = 50
+k4 = 50
+x_in, y_in, x_out, y_out, a1, b1, a2, b2 = keyhole.boundary_points(k1 = k1, k2 = k2, k3 = k3, k4 = k4, seed=42)
 
 # Plotting
 plt.figure(figsize=(6, 6))
